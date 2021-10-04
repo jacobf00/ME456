8cea11209164171653bf2a08a85b8890621d3496
Jacob Foulds <jfoulds00@gmail.com>
2021-10-04 16:32:10 -0400

    add project files

---
+++ Project1/initializeNoise.m
@@ -0,0 +1,6 @@
+noiseMotor = 100;
+noiseEncoder = 50;
+seed1 = round(rem(now,1)*10^8)+1238;
+seed2 = round(rem(now,1)*10^8)+875934857;
+seed3 = round(rem(now,1)*10^8)+63232;
+seed4 = round(rem(now,1)*10^8)+38974023;
